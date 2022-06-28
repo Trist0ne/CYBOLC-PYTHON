bdada41b18f66787821362fda645a91961d90169
Trist0n <tristonrv@gmail.com>
2022-06-28 11:02:11 -0400

    Day 4 Updates

---
+++ Triston Vaira/Day 4 Activities/Activity 21i.py
@@ -0,0 +1,9 @@
+
+def find_product(num1,num2):
+    return(num1*num2)
+    
+
+if __name__=='__main__':
+    num1 = int(input("Please enter your first number: "))
+    num2 = int(input("Please enter your second number: "))
+    print(find_product(num1,num2))
--- Triston Vaira/Day 4 Activities/Activity 22.py
+++ Triston Vaira/Day 4 Activities/Activity 22.py
@@ -1,2 +1,8 @@
 def round_to_position(lst):
-    
+    rounded = []
+    n = 0
+    for int in lst:
+        rounded_number = round(int, n)
+        rounded.append(rounded_number)
+        n = n+1
+    return(rounded)
